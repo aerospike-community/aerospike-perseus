751e439e79b937024e28674a85d414f55d02e584
Behrad Babaee <behrad@aerospike.com>
2023-06-14 12:47:13 +0100

    Config changes

--- aerolab/client/templates/perseus_setup.sh
+++ aerolab/client/templates/perseus_setup.sh
@@ -4,9 +4,9 @@ tar -xvf openjdk-18.0.2_linux-aarch64_bin.tar.gz
 sudo mv jdk-18.0.2 /opt/
 export JAVA_HOME=/opt/jdk-18.0.2
 export PATH=$PATH:$JAVA_HOME/bin
-wget https://dlcdn.apache.org/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.tar.gz -P /tmp
+wget https://dlcdn.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz -P /tmp
 sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
-sudo ln -s /opt/apache-maven-3.9.1 /opt/maven
+sudo ln -s /opt/apache-maven-3.8.8 /opt/maven
 PATH="/opt/maven/bin:$PATH"
 echo "export PATH=$PATH:/opt/jdk-18/bin:/opt/maven/bin" >> ~/.bashrc
 
