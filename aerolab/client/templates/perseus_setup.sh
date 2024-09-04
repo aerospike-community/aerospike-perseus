bc1cb6f3e1cecb615ec2f245a43afdfa6d2bbb8f
Behrad Babaee <behradbabaee@192.168.1.123>
2024-09-04 11:50:54 +0100

    pull before build

--- aerolab/client/templates/perseus_setup.sh
+++ aerolab/client/templates/perseus_setup.sh
@@ -6,6 +6,7 @@ sudo apt install -y maven
 cd /root
 git clone https://github.com/behrockz/aerospike-perseus
 cd /root/aerospike-perseus
+git pull
 mvn package
 
 echo "Setup of Perseus is finished!"
