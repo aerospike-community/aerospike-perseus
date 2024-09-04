e71c789460a8298cac5c8725ed21e47f2e7f820a
Behrad Babaee <behradbabaee@192.168.1.123>
2024-09-04 21:47:43 +0100

    increasing the size of the heap

--- aerolab/aws/configure.sh
+++ aerolab/aws/configure.sh
@@ -22,11 +22,11 @@ CLIENT_INSTANCE_TYPE="c6a.32xlarge"
 CLIENT_NUMBER_OF_NODES=1
 
 # Perseus Additional Testcases
-KEY_CACHE_CAPACITY=1000000000 #The instance must have enough RAM to keep the key cache in memory. Each entry is 8 Bytes. 1 billion entries need 8 GB of Ram
-KEY_CACHE_SAVE_RATIO=.5
+KEY_CACHE_CAPACITY=500000000 #The instance must have enough RAM to keep the key cache in memory. Each entry is 8 Bytes. 1 billion entries need 8 GB of Ram
+KEY_CACHE_SAVE_RATIO=0.5
 TRUNCATE_SET=True
-STRING_INDEX=False
-NUMERIC_INDEX=False
+STRING_INDEX=True
+NUMERIC_INDEX=True
 GEO_SPATIAL_INDEX=False
 UDF_AFFREGATION=False
 
