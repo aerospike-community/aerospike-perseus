54c652d292ee551a8b713a5c1c7ecc531e4035f9
Behrad Babaee <behradbabaee@Behrads-MacBook-Pro.local>
2024-09-05 09:25:39 +0100

    Fixing stats.

--- aerolab/aws/configure.sh
+++ aerolab/aws/configure.sh
@@ -22,8 +22,8 @@ CLIENT_INSTANCE_TYPE="c6a.32xlarge"
 CLIENT_NUMBER_OF_NODES=1
 
 # Perseus Additional Testcases
-KEY_CACHE_CAPACITY=500000000 #The instance must have enough RAM to keep the key cache in memory. Each entry is 8 Bytes. 1 billion entries need 8 GB of Ram
-KEY_CACHE_SAVE_RATIO=0.5
+KEY_CACHE_CAPACITY=1000000000 #The instance must have enough RAM to keep the key cache in memory. Each entry is 8 Bytes. 1 billion entries need 8 GB of Ram
+KEY_CACHE_SAVE_RATIO=1.0
 TRUNCATE_SET=True
 STRING_INDEX=True
 NUMERIC_INDEX=True
