7bcc81d34ce87a6ce880459c214771b340e544ea
Behrad Babaee <behradbabaee@Behrads-MacBook-Pro.local>
2024-12-19 17:35:59 +0000

    update readme to include all the recent changes. Also adds expiry.

--- aerolab/aws/configure.sh
+++ aerolab/aws/configure.sh
@@ -1,20 +1,21 @@
 # AWS Detail
-AWS_REGION="eu-west-1"
+AWS_REGION="eu-central-1"
+AWS_EXPIRE="1h" # length of life of nodes prior to expiry; seconds, minutes, hours, ex 20h 30m. 0 for no expiry.
 
 # Aerospike Detail
-VER="7.1.0.5"
+VER="7.2.0.3"
 NAMESPACE_NAME="Test"
-NAMESPACE_DEFAULT_TTL="1H"
+NAMESPACE_DEFAULT_TTL="0"
 NAMESPACE_STORAGE_TYPE="HMA" #HMA OR MEMORY
 NAMESPACE_REPLICATION_FACTOR=2
 
 # Cluster Detail
 CLUSTER_NAME="Aerospike"
-CLUSTER_NUMBER_OF_NODES="3"
+CLUSTER_NUMBER_OF_NODES="4"
 CLUSTER_INSTANCE_TYPE="i4i.2xlarge"
 CLUSTER_INSTANCE_NUMBER_OF_NVMES="1"
-CLUSTER_INSTANCE_NUMBER_OF_PARTITION_ON_EACH_NVME="4"
-CLUSTER_OVERPROVISIONING_PERCENTAGE=0
+CLUSTER_INSTANCE_NUMBER_OF_PARTITION_ON_EACH_NVME="5"
+CLUSTER_OVERPROVISIONING_PERCENTAGE=15
 
 # GRAFANA Detail
 GRAFANA_NAME=${CLUSTER_NAME}"_GRAFANA"
@@ -22,7 +23,7 @@ GRAFANA_INSTANCE_TYPE="t3.xlarge"
 
 # Client Detail
 CLIENT_NAME="Perseus_${CLUSTER_NAME}"
-CLIENT_INSTANCE_TYPE="c6a.32xlarge" #Choose instances with more cpus, more than 32 GB of RAM, and no NVMe. C6a family are good choices.
+CLIENT_INSTANCE_TYPE="c6a.16xlarge" #Choose instances with more cpus, more than 32 GB of RAM, and no NVMe. C6a family are good choices.
 CLIENT_NUMBER_OF_NODES=1
 
 # Testcases
@@ -33,10 +34,10 @@ UDF_AFFREGATION=False
 TRUNCATE_SET=False
 
 # Workload Details
-RECORD_SIZE=1000 #Bytes
-BATCH_READ_SIZE=50
+RECORD_SIZE=2000 #Bytes
+BATCH_READ_SIZE=100
 BATCH_WRITE_SIZE=50
-READ_HIT_RATIO=0.8
+READ_HIT_RATIO=0.999
 
 # Perseus Detail
 KEY_CACHE_CAPACITY=1000000000 #The instance must have enough RAM to keep the key cache in memory. Each entry is 8 Bytes. 1 billion entries need 8 GB of Ram
