b1340991f1223a193dc158343527258bbe5c377d
Behrad Babaee <behradbabaee@192.168.1.123>
2025-01-17 17:09:23 +0000

    fine tune average size 1

--- src/main/java/com/aerospike/perseus/data/generators/DummyStringGenerator.java
+++ src/main/java/com/aerospike/perseus/data/generators/DummyStringGenerator.java
@@ -6,7 +6,7 @@ public class DummyStringGenerator extends BaseGenerator<String> {
     private final double size;
 
     public DummyStringGenerator(double size) {
-        double temp = size - 400;
+        double temp = size - 158;
         if(temp < 0){
             temp = 0;
         }
@@ -24,7 +24,7 @@ public class DummyStringGenerator extends BaseGenerator<String> {
             return "";
 
         int numberOfBytes = (int) Math.ceil(
-                random.nextGaussian(size, size / 10));
+                random.nextGaussian(size, 5));
         if(numberOfBytes < 0 )
             return "";
         byte[] array = new byte[numberOfBytes];
